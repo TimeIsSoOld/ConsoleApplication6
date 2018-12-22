a41f040ecb76468d3463a0cccf3b359c56a305e9
Administrator <Administrator@PC-20170512LPMV>
2018-12-22 16:26:25 +0800

    1

--- ConsoleApplication4/Program.cs
+++ ConsoleApplication4/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HelloWrold");///qeqe
+            Console.WriteLine("HelloWrold");///
         }
     }
 }
